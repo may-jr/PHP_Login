fdbc434824e4b4482290d76acd36b57c4de440d0
Manasseh Kabutey <107959408+Qharny@users.noreply.github.com>
2024-03-12 07:23:09 +0000

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -1,63 +1,121 @@
+// <?php
+// session_start();
+// // database connection info.
+// $DATABASE_HOST = 'localhost';
+// $DATABASE_USER = 'root';
+// $DATABASE_PASS = '';
+// $DATABASE_NAME = 'phplogin';
+// // Try and connect using the info above.
+// $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+// if (mysqli_connect_errno()) {
+//     // If there is an error with the connection, stop the script and display the error.
+//     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+// }
+// // the code will start the session as this enables to preserve the account details on the server
+// // and will be used later on to remember loggin-in users.
+
+// //checking if the data from the login was submitted, isset() will check if the data exists.
+// if(!isset($_POST['username'],$_POST['password'])){
+//     //could not get the data that should have been sent
+//     exit('please fill both username and password fields!');
+
+// }
+
+// // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
+// if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+//     // Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+//     $stmt->bind_param('s', $_POST['username']);
+//     $stmt->execute();
+//     // Store the result so we can check if the account exists in the database.
+//     $stmt->store_result();
+
+
+//     $stmt->close();
+// }
+
+// if ($stmt->num_rows($con) > 0) {
+//     $stmt->bind_result($id, $password);
+//     $stmt->fetch();
+//     // Account exists, now we verify the password.
+//     // Note: remember to use password_hash in your registration file to store the hashed passwords.
+//     if (password_verify($_POST['password'], $password)) {
+//         // Verification success! User has logged-in!
+//         // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
+//         session_regenerate_id();
+//         $_SESSION['loggedin'] = TRUE;
+//         $_SESSION['name'] = $_POST['username'];
+//         $_SESSION['id'] = $id;
+//         // echo 'Welcome back, ' . htmlspecialchars($_SESSION['name'], ENT_QUOTES) . '!';
+//         header("Location: home.php");
+//     } else {
+//         // Incorrect password
+//         echo 'Incorrect username and/or password!';
+//     }
+// } else {
+//     // Incorrect username
+//     echo 'Incorrect username and/or password!';
+// }
+
+// // in casse you don't want to use any password encryption method, you can simply replace theff code:
+// // if(password_verify($post['password],$password)){}//
+// // with if ($_POST['password'] === $password) {
+// ?>
+
+
 <?php
 session_start();
-// database connection info.
+
+// Database connection info.
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
 $DATABASE_NAME = 'phplogin';
-// Try and connect using the info above.
+
+// Try to establish a database connection.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
     // If there is an error with the connection, stop the script and display the error.
     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
-// the code will start the session as this enables to preserve the account details on the server
-// and will be used later on to remember loggin-in users.
 
-//checking if the data from the login was submitted, isset() will check if the data exists.
-if(!isset($_POST['username'],$_POST['password'])){
-    //could not get the data that should have been sent
-    exit('please fill both username and password fields!');
-
+// Checking if the login data was submitted.
+if (!isset($_POST['username'], $_POST['password'])) {
+    // Data was not sent, prompt the user to fill both fields.
+    exit('Please fill both username and password fields!');
 }
 
-// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
+// Prepare our SQL statement to prevent SQL injection.
 if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-    // Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+    // Bind parameters (s = string), in this case, the username is a string so we use "s".
     $stmt->bind_param('s', $_POST['username']);
     $stmt->execute();
     // Store the result so we can check if the account exists in the database.
     $stmt->store_result();
 
-
-    $stmt->close();
-}
-
-if ($stmt->num_rows($con) > 0) {
-    $stmt->bind_result($id, $password);
-    $stmt->fetch();
-    // Account exists, now we verify the password.
-    // Note: remember to use password_hash in your registration file to store the hashed passwords.
-    if (password_verify($_POST['password'], $password)) {
-        // Verification success! User has logged-in!
-        // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
-        session_regenerate_id();
-        $_SESSION['loggedin'] = TRUE;
-        $_SESSION['name'] = $_POST['username'];
-        $_SESSION['id'] = $id;
-        // echo 'Welcome back, ' . htmlspecialchars($_SESSION['name'], ENT_QUOTES) . '!';
-        header("Location: home.php");
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($id, $password);
+        $stmt->fetch();
+        // Account exists, now we verify the password.
+        if (password_verify($_POST['password'], $password)) {
+            // Verification success! User has logged in!
+            // Create sessions to remember the user is logged in.
+            session_regenerate_id();
+            $_SESSION['loggedin'] = TRUE;
+            $_SESSION['username'] = $_POST['username'];
+            $_SESSION['id'] = $id;
+            // Redirect to the home page.
+            header("Location: home.php");
+            exit;
+        } else {
+            // Incorrect password.
+            echo 'Incorrect username and/or password!';
+        }
     } else {
-        // Incorrect password
+        // Incorrect username.
         echo 'Incorrect username and/or password!';
     }
-} else {
-    // Incorrect username
-    echo 'Incorrect username and/or password!';
+    $stmt->close();
 }
-
-// in casse you don't want to use any password encryption method, you can simply replace theff code:
-// if(password_verify($post['password],$password)){}//
-// with if ($_POST['password'] === $password) {
 ?>
+
 
